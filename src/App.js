26960cdf45f5a301e267c868bcdfb230c76ce870
Kayushkin <vvkayushkin@gmail.com>
2021-05-22 20:49:59 +0000

    react setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
-import TodoList from './TodoList'
+
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
-    <>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
-    </>
+    <h1>Hello World</h1>
   )
 }
 
--- src/Todo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Todo({ todoJSFile }) {
-    return (
-        <div>
-            {todoJSFile}
-        </div>
-    )
-}
